d87e63a49a16885e4ae80865278b1b3527ac8003
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-15 12:43:43 -0500

    receive an image as a file was added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import React, { useState } from 'react';
 import storage from './firebase/firebase'
 
 function App() {
+  const allInputs = { imgUrl: '' }
+  const [imageAsFile, setImageAsFile] = useState('')
+  const [imageAsUrl, setImageAsUrl] = useState(allInputs)
+
+  const handleImageAsFile = e => {
+    console.log(e);
+    const image = e.target.files[0]
+    setImageAsFile(imageFile => (image))
+  }
+
   return (
     <div className="App">
-      blank and ready for image upload
+      <form>
+        <input
+          type="file"
+          onChange={handleImageAsFile}
+        />
+      </form>
     </div>
   );
 }
