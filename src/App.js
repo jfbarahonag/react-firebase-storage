1c3e23e0aa7b4c8805193bde532037988fc08bf5
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-15 23:47:02 -0500

    upload multiple images at the time was implemented

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
 import './App.css';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import storage from './firebase/firebase'
 
 function App() {
-  const allInputs = { imgUrl: '' }
-  const [imageAsFile, setImageAsFile] = useState('')
-  const [imageAsUrl, setImageAsUrl] = useState(allInputs)
+  const allInputs = []
+  
+  const [imagesAsFiles, setImagesAsFiles] = useState(allInputs)
+  const [imagesAsUrls, setImagesAsUrls] = useState(allInputs)
+
+  useEffect(() => {
+    // console.log(imagesAsFiles)
+  },)
 
   const handleImageAsFile = e => {
-    console.log(e);
-    const image = e.target.files[0]
-    setImageAsFile(imageFile => (image))
+    for (let index = 0; index < e.target.files.length; index++) {
+      
+      const image = e.target.files[index]
+      
+      if (image.type !== 'image/jpeg')
+      {
+        console.log(`Type not valid -> ${image.name}, ignored`);
+        continue
+      }
+
+      setImagesAsFiles(prevImages => {
+        const current = [...prevImages, image]
+        return current
+      })
+    }
   }
 
   const handleFireBaseUpload = e => {
     e.preventDefault()
     console.log(`Start upload`);
-    if (imageAsFile === '') {
-      console.error(`Not an image, the image file is a ${typeof (imageAsFile)}`);
+
+    // console.log(imagesAsFiles)
+    
+    // check files list
+    if (imagesAsFiles.length <= 0) {
+      console.error(`No image was selected`);
       return
     }
-    const storageRef = ref(storage, `/images/${imageAsFile.name}`)
-    uploadBytes(storageRef, imageAsFile)
-      .then(snapshot => {
-        getDownloadURL(storageRef)
-          .then(url => {
-            console.log(url);
-          })
-      })
+    for (const image of imagesAsFiles) {
+      const storageRef = ref(storage, `/images/${image.name}`)
+      //TODO: Add security to prevent repeat multiple images with the same name
+      uploadBytes(storageRef, image)
+        .then(snapshot => {
+          getDownloadURL(storageRef)
+            .then(url => {
+              console.log(url)
+              // setImagesAsUrls(prevUrls => {
+              //   return([...prevUrls, url])
+              // })
+            })
+        })
+    }
+    
   }
 
   return (
@@ -37,6 +65,7 @@ function App() {
       <form onSubmit={ handleFireBaseUpload }>
         <input
           type="file"
+          multiple
           onChange={handleImageAsFile}
         />
         <button>
